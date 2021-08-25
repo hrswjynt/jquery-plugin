70d7c361fa27fb3c014e8bf3d2ba249a0a93255a
Haris Wijayanto <hrswjynto@gmail.com>
2021-08-25 10:49:31 +0700

    Add slide lima, enam dan menengahkan bullet

--- scripts/custom.js
+++ scripts/custom.js
@@ -3,22 +3,22 @@ var sliding = false;
 
 $(document).ready(function () {
 	var options = {
-		slides: ".slide",
-		swipe: false,
-		transition: "slide",
-		slideTracker: true,
-		slideTrackerID: "slideposition",
-		slideOnInterval: false,
-		interval: 9000,
-		animateDuration: 1000,
-		animationEasing: "ease",
-
-
-
-
-		pauseOnHover: false,
-		magneticSwipe: true,
-		neverEnding: true,
+		slides: ".slide", // Nama slide di slidecontainer
+		swipe: false, // handler swipe, wajib include touchSwipe
+		transition: "slide", // Transisi slide => slide dan fade
+		slideTracker: true, // menambah pelacakan slide
+		slideTrackerID: "slideposition", // Nama pelacakan slide
+		slideOnInterval: false, // Interval slide
+		interval: 9000, // Interval slide, jika slideOnInterval is enabled/true
+		animateDuration: 1000, // Durasi animasi
+		animationEasing: "ease", // Nilai yang diterima: linear ease in out in-out snap easeOutCubic
+		// easeInOutCubic easeInCirc easeOutCirc easeInOutCirc easeInExpo easeOutExpo
+		// easeInOutExpo easeInQuad easeInOutQuad easeInQuart easeOutQuart
+		// easeInOutQuart easeInQuint easeOutQuint easeInOutQuint easeInSine easeOutSine
+		//easeInOutSine easeInBack easeOutBack easeInOutBack
+		pauseOnHover: false, // Pause jika user mengarahkan kursor ke slide container
+		magneticSwipe: true, // efek menempel pada slide ketika swipping/dragging
+		neverEnding: true, // aktifkan untuk membuat efek yang tidak pernah berhenti/neverending.
 	};
 
 	$(".slider").simpleSlider(options);
@@ -58,6 +58,8 @@ $(document).ready(function () {
 	$(".slide#dua").backstretch("./images/image2.jpg")
 	$(".slide#tiga").backstretch("./images/image3.jpg")
 	$(".slide#empat").backstretch("./images/image4.jpg")
+	$(".slide#lima").backstretch("./images/image5.jpg")
+	$(".slide#enam").backstretch("./images/image6.jpg")
 
 	$('.slide .backstretch img').on('dragstart', function (event) { event.preventDefault(); });
 
